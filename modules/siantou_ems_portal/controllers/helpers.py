50af4512100613752d3bc4e690320014e5d3559d
Thierry Fokou <thierry.fokou@iccsoft.com>
2024-12-07 20:28:58 +0100

    Modification de l'affichage de l'emploi du temps

--- modules/siantou_ems_portal/controllers/helpers.py
+++ modules/siantou_ems_portal/controllers/helpers.py
@@ -22,17 +22,6 @@ CURRENT_HOUR = [
     '15.0-17.0',
 ]
 
-CURRENT_WEEKDAY = {
-    'Heure': CURRENT_HOUR,
-    'Lundi': [],
-    'Mardi': [],
-    'Mercredi': [],
-    'Jeudi': [],
-    'Vendredi': [],
-    'Samedi': [],
-    'Dimanche': [],
-}
-
 _logger = logging.getLogger(__name__)
 
 class Helpers:
@@ -66,8 +55,6 @@ class Helpers:
             user = http.request.env.user.employee_id
             search_domain.append(('employee_id', '=', user.id))
 
-            _logger.info(f'----------- tototototototo search_domain {search_domain} -----------')
-
             timetables = http.request.env['siantou.ems.timetable.timetable'].sudo().search(search_domain, order=order)
             timetables = list(timetables)
             search_timetables = timetables
@@ -79,8 +66,6 @@ class Helpers:
                 # Si l'étudiant est trouvé, on filtre par cycle, niveau et filière
                 search_domain.append(('level_id', '=', student.level_id.id))
                 search_domain.append(('field_of_study_id', '=', student.field_of_study_id.id))
-
-                _logger.info(f'----------- tototototototo search_domain {search_domain} -----------')
 
                 timetables = http.request.env['siantou.ems.timetable.timetable'].sudo().search(search_domain, order=order)
                 timetables = list(timetables)
@@ -117,25 +102,19 @@ class Helpers:
             # Si l'étudiant est trouvé, on filtre par cycle, niveau et filière
             search_domain.append(('student_id', '=', student.id))
 
-            _logger.info(f'----------- tototototototo search_domain 1 {search_domain} -----------')
-
             schoolfees = http.request.env['education.fee.payment'].sudo().search(search_domain, order=order)
             schoolfees = list(schoolfees)
             search_schoolfees = schoolfees
-
-            _logger.info(f'----------- tototototototo schoolfees 1 {schoolfees} -----------')
 
             search_domain = []
 
             search_domain.append(('student_id', '=', student.student_enroll_id.id))
 
-            _logger.info(f'----------- tototototototo search_domain 2 {search_domain} -----------')
-
             schoolfees = http.request.env['education.fee.payment.enrollment'].sudo().search(search_domain, order=order)
             schoolfees = list(schoolfees)
             search_schoolfees += schoolfees
 
-            _logger.info(f'----------- tototototototo schoolfees 2 {schoolfees} -----------')
+        _logger.info(f'----------- tototototototo search_schoolfees {search_schoolfees} -----------')
 
         return search_schoolfees, searchbar_inputs, search_in, sortby, searchbar_sortings
 
@@ -163,25 +142,34 @@ class Helpers:
             user = http.request.env.user.employee_id
             search_domain.append(('employee_id', '=', user.id))
 
-            _logger.info(f'----------- tototototototo search_domain {search_domain} -----------')
-
             paymenthistories = http.request.env['hr.payslip'].sudo().search(search_domain, order=order)
             paymenthistories = list(paymenthistories)
             search_paymenthistories = paymenthistories
 
-            _logger.info(f'----------- tototototototo search_paymenthistories {search_paymenthistories} -----------')
+        _logger.info(f'----------- tototototototo search_paymenthistories {search_paymenthistories} -----------')
 
         return search_paymenthistories, searchbar_inputs, search_in, sortby, searchbar_sortings
 
     @staticmethod
     def format_timetable(data):
-        for hour in CURRENT_HOUR:
-            for key in CURRENT_WEEKDAY.keys():
+        timetables = {
+            'Heure': [hour for hour in CURRENT_HOUR],
+            'Lundi': [],
+            'Mardi': [],
+            'Mercredi': [],
+            'Jeudi': [],
+            'Vendredi': [],
+            'Samedi': [],
+            'Dimanche': [],
+        }
+
+        for hour in timetables['Heure']:
+            for key in timetables.keys():
                 if key == 'Heure':
                     continue
-                CURRENT_WEEKDAY[key].append(np.nan)
+                timetables[key].append(np.nan)
 
-        df = pd.DataFrame(CURRENT_WEEKDAY, dtype=str)
+        df = pd.DataFrame(timetables, dtype=str)
 
         for d in data:
             d['start_time'] = float(d['start_time'])
@@ -189,10 +177,10 @@ class Helpers:
             while d['start_time'] < d['end_time']:
                 h = '{}-{}'.format(d['start_time'], (d['start_time'] + 2.0))
                 for i, row in df.iterrows():
-                    if h == CURRENT_HOUR[i]:
+                    if h == timetables['Heure'][i]:
                         for j, column in enumerate(df.columns):
-                            dt = datetime.strptime(d['date'], DATE_FORMAT)
-                            for k, key in enumerate(CURRENT_WEEKDAY.keys()):
+                            dt = d['date']
+                            for k, key in enumerate(timetables.keys()):
                                 if k == dt.weekday() + 1:
                                     if column == key:
                                         # df.loc[i, column] = d['subject_name']
@@ -202,13 +190,15 @@ class Helpers:
 
         df.replace(np.nan, '-', inplace=True)
 
-        for key in CURRENT_WEEKDAY.keys():
-            CURRENT_WEEKDAY[key] = list(df[key])
+        for key in timetables.keys():
+            timetables[key] = list(df[key])
             if key != 'Heure':
-                for i, v in enumerate(CURRENT_WEEKDAY[key]):
-                    if v != '-':
-                        CURRENT_WEEKDAY[key][i] = [d for d in data if d['id'] == int(v)][0]['subject_name']
+                for i, v in enumerate(timetables[key]):
+                    if v == '-':
+                        timetables[key][i] = ''
+                    else:
+                        timetables[key][i] = [d for d in data if d['id'] == int(v)][0]
 
-            _logger.info(f'----------- tototototototo CURRENT_WEEKDAY {CURRENT_WEEKDAY} -----------')
+        _logger.info(f'----------- tototototototo timetables {timetables} -----------')
 
-        return CURRENT_WEEKDAY
+        return timetables
