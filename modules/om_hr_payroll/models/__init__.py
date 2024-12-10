61608e843e0dc56a562799bcccaa6210b9610432
Thierry Fokou <thierry.fokou@iccsoft.com>
2024-12-10 16:00:42 +0100

    Ajout des notifications des enseignants sur les abscences concernant leur emploie du temps

--- modules/om_hr_payroll/models/__init__.py
+++ modules/om_hr_payroll/models/__init__.py
@@ -7,3 +7,4 @@ from . import res_config_settings
 from . import hr_salary_rule
 from . import hr_payslip
 from . import resource_mixin
+from . import timetable_notification
---
+++ modules/om_hr_payroll/models/timetable_notification.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+
+import babel
+from datetime import date, datetime, timedelta, time
+from dateutil.relativedelta import relativedelta
+from pytz import timezone
+from odoo import api, fields, models, tools, _
+from odoo.exceptions import UserError, ValidationError
+import pytz
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class TimetableNotification(models.Model):
+    _name = 'timetable.notification'
+    _description = 'Timetable Notification'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
+    template = fields.Char(string='Template')
+    timetable_id = fields.Many2one('siantou.ems.timetable.timetable', string='Emplois du temps')
+    date = fields.Date(string='Date',
+        default=lambda r: date.today(),)
+
+    status = fields.Selection([
+        ('0', 'En attente'),
+        ('1', 'Terminé'),
+    ], 'Statut',
+        default='0',
+    )
+
+    # Contrainte logique pour s'assurer que les heures de début et de fin sont définies
+    @api.constrains('template')
+    def _check_template(self):
+        for record in self:
+            if not record.template or record.template == '':
+                raise ValidationError("Vous devez définir un template")
