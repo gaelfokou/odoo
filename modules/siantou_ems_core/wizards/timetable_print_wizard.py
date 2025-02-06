3fc84f60b2da7f2629ab0af3a563baa836ec3876
Thierry Fokou <thierry.fokou@iccsoft.com>
2025-02-06 13:27:16 +0100

    Modification de la fonction de génération automatique de l'emploi du temps

--- modules/siantou_ems_core/wizards/timetable_print_wizard.py
+++ modules/siantou_ems_core/wizards/timetable_print_wizard.py
@@ -299,8 +299,8 @@ class TimetablePrintWizard(models.TransientModel):
                     h = [hours[j] for j in range(len(hours)) if data[i]['start_time'] == hours[j][0]]
                     if len(h) > 0:
                         data1 = copy.deepcopy(data[i])
-                        data1['end_time'] = h[1]
                         data2 = copy.deepcopy(data[i])
+                        data1['end_time'] = h[1]
                         data1['start_time'] = h[1]
                         current_data.append(data1)
                         current_data.append(data2)
@@ -308,8 +308,8 @@ class TimetablePrintWizard(models.TransientModel):
                         h = [hours[j] for j in range(len(hours)) if data[i]['end_time'] == hours[j][1]]
                         if len(h) > 0:
                             data1 = copy.deepcopy(data[i])
-                            data1['end_time'] = h[0]
                             data2 = copy.deepcopy(data[i])
+                            data1['end_time'] = h[0]
                             data1['start_time'] = h[0]
                             current_data.append(data1)
                             current_data.append(data2)
