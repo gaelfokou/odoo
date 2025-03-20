62238c2444fec28509f9550ad01470e0a6f61ec3
Thierry Fokou <thierry.fokou@iccsoft.com>
2025-03-20 14:06:11 +0100

    Modification du formulaire de création de l'emploi du temps

--- modules/siantou_ems_core/models/res_user.py
+++ modules/siantou_ems_core/models/res_user.py
@@ -1,4 +1,9 @@
-from odoo import fields, models
+
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+from odoo import models, fields, api
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
@@ -28,3 +33,48 @@ class ResUsers(models.Model):
         """
         groups = self.get_groups_for_poste()
         return [('id', 'in', groups.ids)]
+
+    def create_user_employee_or_student(self, user):
+        employee_id = self.env['hr.employee'].search([
+            ('work_email', '=', user.login),
+        ], limit=1)
+        if employee_id:
+            password = employee_id.identifier
+            if employee_id.is_teacher:
+                group_id = self.env.ref('base.group_portal')
+                user.write({
+                    'password' : password,
+                    'groups_id': [(6, 0, [group_id.id])],
+                })
+            else:
+                group_id = self.env.ref('base.group_user')
+                user.write({
+                    'password' : password,
+                    'groups_id': [(6, 0, [group_id.id])],
+                })
+            employee_id.write({
+                'user_id': user.id,
+            })
+        else:
+            student_id = self.env['oe.school.student'].search([
+                ('email', '=', user.login),
+            ], limit=1)
+            if student_id:
+                password = student_id.matricule
+                group_id = self.env.ref('base.group_portal')
+                user.write({
+                    'password' : password,
+                    'groups_id': [(6, 0, [group_id.id])],
+                })
+                student_id.write({
+                    'user_id': user.id,
+                })
+
+    @api.model
+    def create(self, vals):
+        # Création de l'utilisateur
+        user = super().create(vals)
+
+        self.create_user_employee_or_student(user)
+
+        return user
