f6846291db8b831fa7da51cebd01efee42c44928
Fokou Thierry <gaelfokou@hotmail.com>
2025-04-10 03:11:46 +0100

    Modification du formulaire de création de l'emploi du temps

--- modules/siantou_ems_core/__manifest__.py
+++ modules/siantou_ems_core/__manifest__.py
@@ -90,7 +90,7 @@
         'data/student_sequence.xml',
         'data/action_server_student.xml',
         'views/student_component_views.xml',
-        'views/data_request_wizard_views.xml',
+        'wizard/data_request_wizard_views.xml',
     ],
     'license': 'LGPL-3',
     'installable': True,
