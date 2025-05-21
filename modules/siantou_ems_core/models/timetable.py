809a4ca99eae9665b95b8fb1fceede290b096deb
Fokou Thierry <gaelfokou@hotmail.com>
2025-05-21 15:50:11 +0100

    Modification du formulaire de création de l'emploi du temps

--- modules/siantou_ems_core/models/timetable.py
+++ modules/siantou_ems_core/models/timetable.py
@@ -472,20 +472,20 @@ class Timetable(models.Model):
                 record.day_of_week = None
 
     # Contrainte logique pour se rassurer que deux cours ne sont pas programmés dans la même salle de classe sur des horaires qui se chevauchent le même jour
-    @api.constrains('classroom_id', 'date', 'subject_id', 'level_id', 'start_time', 'end_time')
-    def _constrains_classroom_is_free(self):
-        for record in self:
-            timetables = self.search([
-                ('id', '!=', record.id),
-                ('classroom_id', '=', record.classroom_id.id),
-                ('date', '=', record.date),
-                '|',
-                ('subject_id', '!=', record.subject_id.id),
-                ('level_id', '!=', record.level_id.id),
-            ]).filtered(lambda rec: not (rec.start_time >= record.end_time or rec.end_time <= record.start_time))
-            timetables = list(timetables)
-            if len(timetables) > 0:
-                raise ValidationError("Deux cours ne doivent pas être programmés dans la même salle de classe sur des horaires qui se chevauchent le même jour")
+    # @api.constrains('classroom_id', 'date', 'subject_id', 'level_id', 'start_time', 'end_time')
+    # def _constrains_classroom_is_free(self):
+    #     for record in self:
+    #         timetables = self.search([
+    #             ('id', '!=', record.id),
+    #             ('classroom_id', '=', record.classroom_id.id),
+    #             ('date', '=', record.date),
+    #             '|',
+    #             ('subject_id', '!=', record.subject_id.id),
+    #             ('level_id', '!=', record.level_id.id),
+    #         ]).filtered(lambda rec: not (rec.start_time >= record.end_time or rec.end_time <= record.start_time))
+    #         timetables = list(timetables)
+    #         if len(timetables) > 0:
+    #             raise ValidationError("Deux cours ne doivent pas être programmés dans la même salle de classe sur des horaires qui se chevauchent le même jour")
 
     # Contrainte logique pour s'assurer que les heures de début et de fin sont définies et que l'heure de fin est supérieure à l'heure de début
     # @api.constrains('start_time', 'end_time')
