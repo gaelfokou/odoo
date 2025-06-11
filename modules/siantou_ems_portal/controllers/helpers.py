504b02becefecebf2f201539c9541bdc7a0ed93c
Fokou Thierry <gaelfokou@hotmail.com>
2025-06-11 16:55:15 +0100

    Modification du formulaire de création de l'emploi du temps

--- modules/siantou_ems_portal/controllers/helpers.py
+++ modules/siantou_ems_portal/controllers/helpers.py
@@ -684,6 +684,7 @@ class Helpers:
             key_timetable = '{}'.format(d['id'])
             if not key_timetable in subjectsessions:
                 subjectsessions[key_timetable] = {}
+                subjectsessions[key_timetable]['name'] = d['name']
                 subjectsessions[key_timetable]['status'] = 'Effectué' if d['status'] in ['present', 'permission'] else 'En attente'
                 subjectsessions[key_timetable]['class_name'] = d['class_name']
                 subjectsessions[key_timetable]['subject_name'] = d['subject_name']
