b83ab7b2fa56a6bf233f3fda32e80392d0d10b31
Fokou Thierry <gaelfokou@hotmail.com>
2025-07-15 16:38:51 +0100

    Modification du formulaire de création de l'emploi du temps

--- modules/siantou_ems_core/models/menu.py
+++ modules/siantou_ems_core/models/menu.py
@@ -17,82 +17,24 @@ class Menu(models.Model):
             user = self.env.user.employee_id
             if self.env.user.employee_id.is_teacher and self.env.user.employee_id.is_portal:
                 is_user = 'is_portal'
-        _logger.info(f'----------- tototototototo is_user {is_user} -----------')
         if not is_user:
             menu_item_id = self.env.ref('siantou_ems_core.menu_siantou_ems_core_portal').id
             menus.discard(menu_item_id)
         return menus
 
-    def switch_to_portal(self, user):
-        partner_id = user.partner_id
-        company_id = None
-        if user.company_id.id:
-            company_id = user.company_id
-        elif partner_id.company_id.id:
-            company_id = partner_id.company_id
-        employee_id = self.env['hr.employee'].search([
-            ('work_email', '=', user.login),
-        ], limit=1)
-        if employee_id:
-            password = employee_id.identifier
-            employee_id.write({
-                'user_id': user.id,
-                'work_email': user.login,
-            })
-            if employee_id.is_teacher:
-                group_id = self.env.ref('base.group_portal')
-                user.write({
-                    'password' : password,
-                    'groups_id': [(6, 0, [group_id.id])],
-                    'employee_id': employee_id.id,
-                })
-            else:
-                group_id = self.env.ref('base.group_user')
-                user.write({
-                    'password' : password,
-                    'groups_id': [(6, 0, [group_id.id])],
-                    'employee_id': employee_id.id,
-                })
-            partner_id.write({
-                'name': employee_id.name,
-                'email': employee_id.work_email,
-                'phone': employee_id.work_phone,
-                'is_company': False,
-                'company_id': company_id.id,
-                'user_id': user.id,
-                'employee': True,
-            })
-        else:
-            student_id = self.env['oe.school.student'].search([
-                ('email', '=', user.login),
-            ], limit=1)
-            if student_id:
-                password = student_id.matricule
-                student_id.write({
-                    'user_id': user.id,
-                    'email': user.login,
-                })
-                group_id = self.env.ref('base.group_portal')
-                user.write({
-                    'password' : password,
-                    'groups_id': [(6, 0, [group_id.id])],
-                    'student_id': student_id.id,
-                })
-                partner_id.write({
-                    'name': student_id.name,
-                    'email': student_id.email,
-                    'phone': student_id.private_phone,
-                    'is_company': False,
-                    'company_id': company_id.id,
-                    'user_id': user.id,
-                    'employee': False,
-                })
+    def switch_to_portal(self):
+        if self.env.user.employee_id.id:
+            user = self.env.user
+            if self.env.user.employee_id.is_teacher and self.env.user.employee_id.is_portal:
+                is_user = 'is_portal'
+        if is_user:
+            group_user_id = self.env.ref('base.group_user')
+            group_portal_id = self.env.ref('base.group_portal')
+            group_user_id.sudo().write({'users': [(3, user.id)]})
+            group_portal_id.sudo().write({'users': [(4, user.id)]})
 
-    @api.model
-    def create(self, vals):
-        # Création de l'utilisateur
-        user = super(ResUsers, self.with_context(no_reset_password=True)).create(vals)
-
-        self.create_user_employee_or_student(user)
-
-        return user
+        return {
+            'type': 'ir.actions.act_url',
+            'url': 'http://127.0.0.1:8069/my',
+            'target': 'self',
+        }
