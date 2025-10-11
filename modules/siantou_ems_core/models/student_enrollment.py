78190743260a12f93d436406a8b4832d8c543adc
Fokou Thierry <gaelfokou@hotmail.com>
2025-10-11 11:06:16 +0100

    Modification d'une candidature

--- modules/siantou_ems_core/models/student_enrollment.py
+++ modules/siantou_ems_core/models/student_enrollment.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from datetime import date, datetime, timedelta, time
-import logging
-
-from psycopg2 import sql, DatabaseError
-
 from odoo import models, fields, api, tools, _
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.exceptions import UserError, ValidationError
 from odoo.addons.base.models.res_partner import WARNING_MESSAGE, WARNING_HELP
+from datetime import date, datetime, timedelta, time
+import re
+import psycopg2
+import logging
 
 _logger = logging.getLogger(__name__)
 
