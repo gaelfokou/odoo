4c128a6ee5e3a5710154d6707f93624fc15bc0de
Fokou Thierry <gaelfokou@hotmail.com>
2025-11-10 18:09:30 +0100

    Ajout de la fonction de vérification des cours émargés

--- modules/om_hr_payroll/salary_rules.py
+++ modules/om_hr_payroll/salary_rules.py
@@ -1,5 +1,5 @@
 # Available variables:
-#----------------------
+# ----------------------
 # payslip: object containing the payslips
 # employee: hr.employee object
 # contract: hr.contract object
@@ -11,24 +11,39 @@
 # Note: returned value have to be set in the variable 'result'
 
 # result = contract.wage * 0.10
-
 # result = worked_days.WORK100.number_of_days
-
 # result = worked_days.WORK100.number_of_hours
 
 # Structure des salaires :
 
-# Salaire enseignant permanent SALENSPER
-# Salaire enseignant temporaire SALENSTEM
-# Salaire employé permanent SALEMPPER
-# Salaire employé temporaire SALEMPTEM
-
+# Structure de salaire (BASE)
 # Règles salariales :
+# Salaire de Base (BASIC) : result = contract.wage
+# Brut (GROSS) : result = categories.BASIC + categories.ALW
+# Impôt sur le revenu (IR) : result = (categories.BASIC + categories.ALW) * 5.5 / 100
+# Apecus (APECUS) : result = 1500
+# Cnps (CNPS) : result = (categories.BASIC + categories.ALW) * 4.2 / 100
+# Salaire Net (NET) : result = categories.BASIC + categories.ALW - categories.DED
 
-# Salaire de base enseignant permanent BASICENSPER
-# Salaire de base enseignant temporaire BASICENSTEM
-# Salaire de base employé permanent BASICEMPPER
-# Salaire de base employé temporaire BASICEMPTEM
+# Structure de salaire chef département (BASECD)
+# Règles salariales :
+# Salaire de Base (BASIC) : result = contract.wage
+# Prime CD (PRIMCD) : result = 0
+# Brut (GROSS) : result = categories.BASIC + categories.ALW
+# Impôt sur le revenu (IR) : result = (categories.BASIC + categories.ALW) * 5.5 / 100
+# Apecus (APECUS) : result = 1500
+# Cnps (CNPS) : result = (categories.BASIC + categories.ALW) * 4.2 / 100
+# Salaire Net (NET) : result = categories.BASIC + categories.ALW - categories.DED
+
+# Structure de salaire coordonnateur (BASECO)
+# Règles salariales :
+# Salaire de Base (BASIC) : result = contract.wage
+# Prime CO (PRIMCO) : result = 0
+# Brut (GROSS) : result = categories.BASIC + categories.ALW
+# Impôt sur le revenu (IR) : result = (categories.BASIC + categories.ALW) * 5.5 / 100
+# Apecus (APECUS) : result = 1500
+# Cnps (CNPS) : result = (categories.BASIC + categories.ALW) * 4.2 / 100
+# Salaire Net (NET) : result = categories.BASIC + categories.ALW - categories.DED
 
 # 8h -- 1jr
 # 40h -- 5jr -- 1week
@@ -37,25 +52,14 @@
 
 # Permanent
 
-worked_days = 0
-
-for line in payslip.worked_days_line_ids:
-    worked_days += line.number_of_days
-
-if worked_days < 20:
-    result = ((contract.wage / 20) * worked_days) - (((contract.wage / 20) * worked_days) * 0.10)
-else:
-    # result = (contract.wage / 20) * worked_days
-    result = contract.wage
+# worked_days = 0
+# for line in payslip.worked_days_line_ids:
+#     worked_days += line.number_of_days
+# result = (contract.wage / 20) * worked_days
 
 # Temporaire
 
-worked_hours = 0
-
-for line in payslip.worked_days_line_ids:
-    worked_hours += line.number_of_hours
-
-if worked_hours < 160:
-    result = ((contract.wage / 160) * worked_hours) - (((contract.wage / 160) * worked_hours) * 0.10)
-else:
-    result = (contract.wage / 160) * worked_hours
+# worked_hours = 0
+# for line in payslip.worked_days_line_ids:
+#     worked_hours += line.number_of_hours
+# result = (contract.wage / 160) * worked_hours
