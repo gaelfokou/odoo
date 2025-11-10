eec72a3baec5bb57b42cbc76a007a809d64f6021
Fokou Thierry <gaelfokou@hotmail.com>
2025-11-10 09:54:46 +0100

    Ajout de la fonction de vérification des cours émargés

--- modules/siantou_ems_portal/controllers/apis.py
+++ modules/siantou_ems_portal/controllers/apis.py
@@ -262,10 +262,10 @@ class ApiAccount(http.Controller):
             paymenthistory['code'] = search_paymenthistory.code
             paymenthistory['contract'] = search_paymenthistory.contract_id.name
             if search_paymenthistory.code:
-                paymenthistory['amount'] = search_paymenthistory.line_ids.filtered(lambda line: line.salary_rule_id.code == search_paymenthistory.code).mapped('amount')[0] if (len(search_paymenthistory.line_ids.ids) > 0 and len(search_paymenthistory.line_ids.filtered(lambda line: line.salary_rule_id.code == search_paymenthistory.code)) > 0) else 0.0
+                paymenthistory['amount'] = search_paymenthistory.line_ids.filtered(lambda line: line.salary_rule_id.code == search_paymenthistory.code).mapped('amount')[0] if (len(list(search_paymenthistory.line_ids)) > 0 and len(search_paymenthistory.line_ids.filtered(lambda line: line.salary_rule_id.code == search_paymenthistory.code)) > 0) else 0.0
             else:
-                paymenthistory['amount'] = search_paymenthistory.line_ids.mapped('amount')[0] if len(search_paymenthistory.line_ids.ids) > 0 else 0.0
-            paymenthistory['number_of_hours'] = sum(search_paymenthistory.worked_days_line_ids.mapped('number_of_hours')) if len(search_paymenthistory.worked_days_line_ids.ids) > 0 else 0.0
+                paymenthistory['amount'] = search_paymenthistory.line_ids.mapped('amount')[0] if len(list(search_paymenthistory.line_ids)) > 0 else 0.0
+            paymenthistory['number_of_hours'] = sum(search_paymenthistory.worked_days_line_ids.mapped('number_of_hours')) if len(list(search_paymenthistory.worked_days_line_ids)) > 0 else 0.0
             paymenthistory['state'] = search_paymenthistory.state
             paymenthistories.append(paymenthistory)
             total_amount += paymenthistory['amount']
