508547420fc88b13e936bab57d168ef4eb9617e7
Fokou Thierry <gaelfokou@hotmail.com>
2025-11-15 12:55:46 +0100

    Ajout de la fonction de vérification des cours émargés

--- modules/siantou_ems_core/models/timetable.py
+++ modules/siantou_ems_core/models/timetable.py
@@ -718,19 +718,6 @@ class Timetable(models.Model):
             if len(timetables) > 0:
                 raise ValidationError("Deux cours ne doivent pas être programmés dans la même classe sur des horaires qui se chevauchent le même jour")
 
-    # Contrainte logique pour se rassurer que deux cours ne sont pas programmés dans la même salle de classe sur des horaires qui se chevauchent le même jour
-    @api.constrains('classroom_id', 'date', 'start_time', 'end_time')
-    def _constrains_classroom(self):
-        for record in self:
-            timetables = self.search([
-                ('id', '!=', record.id),
-                ('classroom_id', '=', record.classroom_id.id),
-                ('date', '=', record.date),
-            ]).filtered(lambda rec: not (rec.start_time >= record.end_time or rec.end_time <= record.start_time))
-            timetables = list(timetables)
-            if len(timetables) > 0:
-                raise ValidationError("Deux cours ne doivent pas être programmés dans la même salle de classe sur des horaires qui se chevauchent le même jour")
-
     def create_timetable(self, timetable):
         try:
             timetables = []
