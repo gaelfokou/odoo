5f2dac80104892c6a2544ca2c0ca8f0008be6409
Fokou Thierry <gaelfokou@hotmail.com>
2025-11-27 14:24:15 +0100

    Ajout de la fonction de vérification des cours émargés

--- modules/om_hr_payroll/models/hr_payslip.py
+++ modules/om_hr_payroll/models/hr_payslip.py
@@ -494,10 +494,12 @@ class HrPayslip(models.Model):
                                 'status': 'exception',
                             })
                         else:
-                            start_time = datetime.strftime(start_time, TIME_FORMAT_FR)
-                            start_time = HrPayslip.convert_time_to_float(start_time)
+                            end_time = HrPayslip.convert_datetime_from_utc(end_time)
+                            start_time = HrPayslip.convert_datetime_from_utc(start_time)
                             end_time = datetime.strftime(end_time, TIME_FORMAT_FR)
+                            start_time = datetime.strftime(start_time, TIME_FORMAT_FR)
                             end_time = HrPayslip.convert_time_to_float(end_time)
+                            start_time = HrPayslip.convert_time_to_float(start_time)
                             employee_timetable.sudo().write({
                                 'worked_start_time': start_time,
                                 'worked_end_time': end_time,
