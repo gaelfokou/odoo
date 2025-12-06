26fc72ed77f6ec4bcb1a22fba2b3928cca615e2a
Fokou Thierry <gaelfokou@hotmail.com>
2025-12-06 10:29:08 +0100

    Ajout de la fonction de vérification des cours émargés

--- modules/om_hr_payroll/models/hr_payslip.py
+++ modules/om_hr_payroll/models/hr_payslip.py
@@ -476,7 +476,7 @@ class HrPayslip(models.Model):
                             'rate': 0.0,
                             'amount': 0.0,
                             'status': 'exception',
-                            'reason': 'Poinçonnement de début ou de fin absent ou non correct',
+                            'reason': 'Poinçonnement de début ou de fin absent ou invalid',
                         })
                     elif len(daily_attendances) > 1:
                         end_punching_time = daily_attendances[1].punching_time
