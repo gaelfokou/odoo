e5ca679b53e8d38c8e7a0a03bc857dbaf40a1924
Fokou Thierry <gaelfokou@hotmail.com>
2026-02-17 16:53:11 +0100

    Ajout de la fonction d'affichage des notes d'examen

--- modules/siantou_ems_core/wizards/class_division_print_wizard.py
+++ modules/siantou_ems_core/wizards/class_division_print_wizard.py
@@ -34,6 +34,12 @@ class ClassPrintWizard(models.TransientModel):
         })
         return report_action.report_action(self, data=data)
 
+    def sort_classe(self, classe):
+        name = classe['name'] if classe['name'] else ''
+        name = name.strip()
+        name = name.lower()
+        return name
+
     def print_class_report_data(self, domains=None):
         # Récupérer les emplois du temps pour le semestre sélectionné
         domain = []
@@ -48,14 +54,18 @@ class ClassPrintWizard(models.TransientModel):
         for search_classe in search_classes:
             classe = {}
             classe['name'] = search_classe.name
-            classe['year'] = search_classe.year_id.name
             classe['school'] = search_classe.school_id.name
+            classe['cycle'] = search_classe.cycle_id.name
+            classe['department'] = search_classe.department_id.name
             classe['field_of_study'] = search_classe.field_of_study_id.name
             classe['level'] = search_classe.level_id.name
             classe['specialty'] = search_classe.specialty_id.name
             classe['option'] = search_classe.option_id.name
+            classe['number_of_student'] = search_classe.number_of_student
             classe['type_cour'] = TYPE_COUR[search_classe.type_cour]
             classes.append(classe)
+
+        classes = sorted(classes, key=self.sort_classe)
 
         filter_title = self.env['ir.config_parameter'].sudo().get_param(f'siantou.filter_user_{self.env.user.id}', '')
 
