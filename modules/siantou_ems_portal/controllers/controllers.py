33301c640755bf69e6405fd8aca50b776c1494b1
Fokou Thierry <gaelfokou@hotmail.com>
2026-03-05 17:24:26 +0100

    Ajout de la fonction d'affichage des notes d'examen

--- modules/siantou_ems_portal/controllers/controllers.py
+++ modules/siantou_ems_portal/controllers/controllers.py
@@ -686,6 +686,7 @@ class PortalAccount(portal.CustomerPortal):
             else:
                 paymenthistory['amount'] = search_paymenthistory.line_ids.mapped('amount')[0] if len(list(search_paymenthistory.line_ids)) > 0 else 0.0
             paymenthistory['number_of_hours'] = sum(search_paymenthistory.worked_days_line_ids.mapped('number_of_hours')) if len(list(search_paymenthistory.worked_days_line_ids)) > 0 else 0.0
+            paymenthistory['number_of_hours'] = round(paymenthistory['number_of_hours'], 2)
             paymenthistory['state'] = STATUS_PAYMENT[search_paymenthistory.state]
             paymenthistories.append(paymenthistory)
             total_amount += paymenthistory['amount']
