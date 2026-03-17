d47ffb5c1bbbd972195fc4c4cc41059f5be50a5a
Fokou Thierry <gaelfokou@hotmail.com>
2026-03-17 12:41:09 +0100

    Ajout de la fonction d'affichage des notes d'examen

--- modules/siantou_ems_core/wizards/timetable_filter_wizard.py
+++ modules/siantou_ems_core/wizards/timetable_filter_wizard.py
@@ -935,7 +935,7 @@ class TimetableFilterWizard(models.TransientModel):
         }
         for key in data.keys():
             all_data['docdata']['label'] = data[key]['docdata']['label']
-            all_data['docdata']['title'] = '{} Comparaison'.format(data[key]['docdata']['title'])
+            all_data['docdata']['title'] = 'Comparaison {}'.format(data[key]['docdata']['title'])
             all_data['docdata']['filter'] = filter_title
             all_data['docdata']['sort_type'] = data[key]['docdata']['sort_type']
             all_data['docdata']['status'] = data[key]['docdata']['status']
