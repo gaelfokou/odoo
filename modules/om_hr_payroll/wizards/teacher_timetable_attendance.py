8660f0be1e7175817c726a9f10aab33d2843f64e
Fokou Thierry <gaelfokou@hotmail.com>
2026-06-24 19:26:16 +0100

    Suivi des requêtes

--- modules/om_hr_payroll/wizards/teacher_timetable_attendance.py
+++ modules/om_hr_payroll/wizards/teacher_timetable_attendance.py
@@ -423,7 +423,7 @@ class TeacherTimetableAttendance(models.TransientModel):
                 debt_ids = list(debt_ids)
                 if len(debt_ids) > 0:
                     for debt_id in debt_ids:
-                        if amount == 0.0:
+                        if amount <= 0.0 or debt_id.rest_amount <= 0.0:
                             break
                         if amount >= debt_id.rest_amount:
                             last_rest_amount = 0.0
