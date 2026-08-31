157b5907f084fb5c3e6f5ce34f49809e05306998
Fokou Thierry <gaelfokou@hotmail.com>
2026-08-31 14:10:03 +0100

    imprimer le tableau de bord

--- modules/siantou_ems_core/models/year.py
+++ modules/siantou_ems_core/models/year.py
@@ -29,24 +29,58 @@ class Year(models.Model):
     # Variable booléenne pour définir une année académique comme étant active (année académique en cours)
     is_active = fields.Boolean(string='Actif ?', default=False)
 
-    @api.constrains('start_time', 'end_time')
+    is_valid = fields.Boolean(string='Valide ?', default=False)
+
+    user_id = fields.Many2one(
+        'res.users',
+        string='Utilisateur associé',
+        help='Utilisateur associé à cet étudiant'
+    )
+
+    @api.constrains('start_time', 'end_time', 'user_id')
     def _check_date(self):
         for record in self:
-            years = self.env['siantou.ems.core.year'].search([('id', '!=', record.id)]).filtered(lambda rec: not (rec.start_time >= record.end_time or rec.end_time <= record.start_time))
-            years = list(years)
-            if len(years) > 0:
-                raise ValidationError('Les dates de l\'année académique ne peuvent se chevaucher')
-            if record.start_time >= record.end_time:
-                raise ValidationError('La date de fin doit être supérieure à la date de début')
-
-    @api.constrains('is_active')
-    def _check_unique_active(self):
-        for record in self:
-            if record.is_active:
+            if record.user_id.id:
                 years = self.env['siantou.ems.core.year'].search([
                     ('id', '!=', record.id),
-                    ('is_active', '=', True),
-                ])
+                    ('user_id', '=', record.user_id.id),
+                ]).filtered(lambda rec: not (rec.start_time >= record.end_time or rec.end_time <= record.start_time))
                 years = list(years)
                 if len(years) > 0:
-                    raise ValidationError(f"Une année académique est déjà active")
+                    raise ValidationError(f'Les dates de l\'année académique ne peuvent se chevaucher pour l\'utilisateur {record.user_id.name}')
+                if record.start_time >= record.end_time:
+                    raise ValidationError('La date de fin doit être supérieure à la date de début')
+            else:
+                years = self.env['siantou.ems.core.year'].search([
+                    ('id', '!=', record.id),
+                    ('user_id', '=', False),
+                ]).filtered(lambda rec: not (rec.start_time >= record.end_time or rec.end_time <= record.start_time))
+                years = list(years)
+                if len(years) > 0:
+                    raise ValidationError(f'Les dates de l\'année académique ne peuvent se chevaucher')
+                if record.start_time >= record.end_time:
+                    raise ValidationError('La date de fin doit être supérieure à la date de début')
+
+    @api.constrains('is_active', 'user_id')
+    def _check_unique_active(self):
+        for record in self:
+            if record.user_id.id:
+                if record.is_active:
+                    years = self.env['siantou.ems.core.year'].search([
+                        ('id', '!=', record.id),
+                        ('user_id', '=', record.user_id.id),
+                        ('is_active', '=', True),
+                    ])
+                    years = list(years)
+                    if len(years) > 0:
+                        raise ValidationError(f'Une année académique est déjà active pour l\'utilisateur {record.user_id.name}')
+            else:
+                if record.is_active:
+                    years = self.env['siantou.ems.core.year'].search([
+                        ('id', '!=', record.id),
+                        ('user_id', '=', False),
+                        ('is_active', '=', True),
+                    ])
+                    years = list(years)
+                    if len(years) > 0:
+                        raise ValidationError(f'Une année académique est déjà active')
